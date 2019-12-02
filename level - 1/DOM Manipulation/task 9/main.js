93f06c3dacb447e9651f9f121872a283206d4f33
zivile-b <zivile.batuleviciute@yahoo.com>
2019-12-03 00:50:30 +0200

    commit'as2

--- level - 1/DOM Manipulation/task 9/main.js
+++ level - 1/DOM Manipulation/task 9/main.js
@@ -3,8 +3,21 @@
 
 function btnClick(){
     //code here
+var gerulis = document.getElementById('input').value;
+document.getElementById('output').innerHTML=gerulis;
 
     
 }
 
+
+// 2.var
+// function btnClick(){
+//     let input = document.getElementById('input');
+//     let output = document.getElementById('output');
+    
+//     let inputText = 'Output: ';
+//     inputText += input.value;
+//     output.innerHTML = inputText;
+
+// }
 //answer is in answer.txt

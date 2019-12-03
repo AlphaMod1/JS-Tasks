c7dfa2c47c79dae9f1d860f87723003256d8f67d
AlphaMod1 <liutauras.vijeikis@gmail.com>
2019-12-03 20:07:44 +0200

    task 10

---
+++ level - 1/DOM Manipulation/task 10/main.js
@@ -0,0 +1,10 @@
+//Generate a chess board of 'n' size inside <div id="board"></div>
+//Size is selected with <input type="text" id="col"> and <input type="text" id="row">
+//For board tiles use classes ".black" and ".white"
+
+function btnClick() {
+    //code here
+
+}
+
+//answer is in answer.txt
